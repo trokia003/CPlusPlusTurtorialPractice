a035e4902a507902d63ff41fd95cf2a7dcdd72cc
Trokia003 <37078152+Trokia003@users.noreply.github.com>
2018-12-18 10:36:50 -0600

    Finished Variable Types
    I have gone through lessons - Home, Overview, Environment Setup, Basic Syntax, Comments, Data Types, and Variable Types.
    
    Right now I am only utilizing a main class for these tutorials.

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,43 @@ using namespace std;
 // main() is where program execution begins.
 int main() {
    cout << "Hello World"; // prints Hello World
+   
+   
+   /* This is a multi-line comment */
+   //this is a single line comment
+   
+   //Boolean
+   bool yes = true;
+   bool no = false;
+   
+   //Character
+   char thisCharacter = 'a';
+   char thatCharacter = 'b';
+   
+   //Integer
+   int bigInt = 214748364;
+   int smallInt = 21;
+   
+   //Floating Point
+   float fewDecimals = 2.1;
+   float manyDecimals = 2.15647;
+   
+   //Double Floating Point
+   double fewDecimalPoints = 2.136;
+   double manyDecimalPoints = 2.15647795486;
+   
+   
+   //multiple variables can be declared and instantiated in the same line
+   int x, y = 2, z =-3;
+   char letter1 = 'c', letter2 = 'a'. letter3;
+   
+   
+   
+   
+   
+   
+   
+   
+   
    return 0;
 }
